e3343b35efffbbb1be3ee591203db16907cb8d30
Steven-Yang914 <yyang914@seas.upenn.edu>
2023-12-03 16:28:16 -0500

    Add more routes

--- server/routes.js
+++ server/routes.js
@@ -49,6 +49,41 @@ const random = async function (req, res) {
   }
 }
 
+const allMovies = async function (req, res) {
+  connection.query(`
+    SELECT *
+    FROM Movies
+    ORDER BY StartYear DESC
+  `, (err, data) => {
+    if (err || data.length === 0) {
+      console.log(err);
+      res.json([]);
+    } else {
+      res.json(data);
+    }
+  });
+}
+
+const movie = async function (req, res) {
+  const movieID = req.params.movie_id;
+
+  connection.query(`
+    SELECT *
+    FROM Movies m, People p, Crew_in c, Ratings r
+    WHERE c.MovieID = m.MovieID
+    AND c.PeopleID = p.PeopleID
+    AND m.MovieID = r.MovieID
+    AND m.MovieID = '${movieID}'
+  `, (err, data) => {
+    if (err || data.length === 0) {
+      console.log(err);
+      res.json([]);
+    } else {
+      res.json(data);
+    }
+  });
+}
+
 const topMovies = async function (req, res) {
   const page = req.query.page;
   const pageSize = req.query.page_size ?? 10;
@@ -90,7 +125,63 @@ const topMovies = async function (req, res) {
   }
 }
 
+const allPeople = async function (req, res) {
+  connection.query(`
+    SELECT *
+    FROM People
+    ORDER BY Name ASC
+  `, (err, data) => {
+    if (err || data.length === 0) {
+      console.log(err);
+      res.json([]);
+    } else {
+      res.json(data);
+    }
+  });
+}
+
+const person = async function (req, res) {
+  const personID = req.params.person_id;
+
+  connection.query(`
+    SELECT *
+    FROM Movies m, People p, Crew_in c
+    WHERE c.MovieID = m.MovieID AND c.PeopleID = p.PeopleID
+    AND p.PeopleID = '${personID}'
+  `, (err, data) => {
+    if (err || data.length === 0) {
+      console.log(err);
+      res.json([]);
+    } else {
+      res.json(data);
+    }
+  });
+}
+
+const getGenreOfMovie = async function (req, res) {
+  const movieID = req.params.movie_id;
+
+  connection.query(`
+    SELECT og.Genre
+    FROM Movies m, ofGenre og
+    WHERE m.MovieID = og.MovieID
+    AND m.MovieID = '${movieID}'
+  `, (err, data) => {
+    if (err || data.length === 0) {
+      console.log(err);
+      res.json([]);
+    } else {
+      res.json(data);
+    }
+  });
+}
+
 module.exports = {
   random,
+  allMovies,
+  movie,
+  getGenreOfMovie,
   topMovies,
+  allPeople,
+  person,
 }
--- server/server.js
+++ server/server.js
@@ -9,7 +9,12 @@ app.use(cors({
 }));
 
 app.get('/random', routes.random);
+app.get('/allMovies', routes.allMovies);
+app.get('/movie/:movie_id', routes.movie);
+app.get('/movie/:movie_id/genres', routes.getGenreOfMovie);
 app.get('/topMovies', routes.topMovies);
+app.get('/allPeople', routes.allPeople)
+app.get('/person/:person_id', routes.person);
 
 app.listen(config.server_port, () => {
   console.log(`Server running at http://${config.server_host}:${config.server_port}/`)
